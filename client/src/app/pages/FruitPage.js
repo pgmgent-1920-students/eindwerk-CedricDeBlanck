c9c470d740dcb5c7eb65742e5eaf7965e0969bfc
CedricDeBlanck <cedridebl1@student.arteveldehs.be>
2020-06-14 17:58:57 +0200

    wip

--- client/src/app/pages/FruitPage.js
+++ client/src/app/pages/FruitPage.js
@@ -2,7 +2,7 @@ import React, { Fragment, useState, useEffect, useRef } from 'react';
 
 import { BookmarksList } from '../components/bookmark';
 
-const API_URL = 'https://raw.githubusercontent.com/CedricDeBlanck/Eindwerk-pgm-BAAS-CedricDeBlanck/master/data/fruit.json?token=ANGVHBUQ77SZ5NHL4U3JAE264YWYSl';
+const API_URL = 'https://github.com/CedricDeBlanck/Eindwerk-pgm-BAAS-CedricDeBlanck/blob/master/data/fruit.json';
 
 const BookmarksPage = ({}) => {
   const refElement = useRef();
