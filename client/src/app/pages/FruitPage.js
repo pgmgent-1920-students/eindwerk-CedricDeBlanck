de0c9d90bc52e2adc772e74ee57cbbece1e3cf74
CedricDeBlanck <cedridebl1@student.arteveldehs.be>
2020-06-14 16:00:44 +0200

    wip

--- client/src/app/pages/FruitPage.js
+++ client/src/app/pages/FruitPage.js
@@ -2,7 +2,7 @@ import React, { Fragment, useState, useEffect, useRef } from 'react';
 
 import { BookmarksList } from '../components/bookmark';
 
-const API_URL = 'https://www.fruityvice.com/api/fruit/all';
+const API_URL = 'https://raw.githubusercontent.com/CedricDeBlanck/Eindwerk-pgm-BAAS-CedricDeBlanck/master/data/fruit.json?token=ANGVHBUQ77SZ5NHL4U3JAE264YWYSl';
 
 const BookmarksPage = ({}) => {
   const refElement = useRef();
