60b38c99783c81adb98de1be6e7ea7dc9a28a613
CedricDeBlanck <cedridebl1@student.arteveldehs.be>
2020-06-16 13:27:13 +0200

    wip

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
+import App from './app';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
 import * as serviceWorker from './serviceWorker';
 
 import 'jquery/dist/jquery.slim.min';
