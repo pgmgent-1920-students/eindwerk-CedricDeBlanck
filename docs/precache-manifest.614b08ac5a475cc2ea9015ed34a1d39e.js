727e9c91c43b889e725b9a85851802486a2f26bb
CedricDeBlanck <cedridebl1@student.arteveldehs.be>
2020-06-16 13:25:26 +0200

    wip

---
+++ docs/precache-manifest.614b08ac5a475cc2ea9015ed34a1d39e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d350f15d07c5508ee259124b9c3795b6",
+    "url": "/react-firebase/home/index.html"
+  },
+  {
+    "revision": "a6aa019789aeef3d2ee0",
+    "url": "/react-firebase/home/static/css/main.db3c5f32.chunk.css"
+  },
+  {
+    "revision": "4ca7a6870db27834ea45",
+    "url": "/react-firebase/home/static/js/2.3d3b4981.chunk.js"
+  },
+  {
+    "revision": "007ad04fbdf73c9ea88306dc278d6468",
+    "url": "/react-firebase/home/static/js/2.3d3b4981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6aa019789aeef3d2ee0",
+    "url": "/react-firebase/home/static/js/main.baaea8ec.chunk.js"
+  },
+  {
+    "revision": "cfc43eb93460c93caa60",
+    "url": "/react-firebase/home/static/js/runtime-main.9da73fef.js"
+  }
+]);
