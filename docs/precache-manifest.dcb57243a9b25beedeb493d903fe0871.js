858fc9f696b0820a43b325f43f6159016e286a4e
CedricDeBlanck <cedridebl1@student.arteveldehs.be>
2020-06-17 16:15:35 +0200

    wip

---
+++ docs/precache-manifest.dcb57243a9b25beedeb493d903fe0871.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "977d2538e44e48b1bac100cebb135806",
+    "url": "/eindwerk-CedricDeBlanck/index.html"
+  },
+  {
+    "revision": "e29a4695d8af91cf2c6f",
+    "url": "/eindwerk-CedricDeBlanck/static/css/main.bc54f320.chunk.css"
+  },
+  {
+    "revision": "887584e2c77f133f1f4f",
+    "url": "/eindwerk-CedricDeBlanck/static/js/2.ce08424f.chunk.js"
+  },
+  {
+    "revision": "007ad04fbdf73c9ea88306dc278d6468",
+    "url": "/eindwerk-CedricDeBlanck/static/js/2.ce08424f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e29a4695d8af91cf2c6f",
+    "url": "/eindwerk-CedricDeBlanck/static/js/main.89b56982.chunk.js"
+  },
+  {
+    "revision": "d3b2979ac3c66b1e11c0",
+    "url": "/eindwerk-CedricDeBlanck/static/js/runtime-main.fe8f1d4c.js"
+  }
+]);
