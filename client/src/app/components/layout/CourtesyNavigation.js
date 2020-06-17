cc5e28066c520eff34a3895e67acc6902b187df3
CedricDeBlanck <cedridebl1@student.arteveldehs.be>
2020-06-17 16:28:45 +0200

    wip

--- client/src/app/components/layout/CourtesyNavigation.js
+++ client/src/app/components/layout/CourtesyNavigation.js
@@ -20,8 +20,8 @@ const CourtesyNavigation = ({children}) => {
               <img className="profile__picture" src={Logo} />
             </button>
             <div className="dropdown-menu dropdown-menu-right">
-              <a className="dropdown-item" href={Routes.LANDING} onClick={signOut}>Logout</a>
-              <a className="dropdown-item" href={Routes.SHOPPINGBASKET} >Winkelwagen</a>
+              <a className="dropdown-item" onClick={signOut}><Link className="nav-link" to={Routes.AUTH_SIGN_OUT}>Log uit</Link></a>
+              <a className="dropdown-item"><Link className="nav-link" to={Routes.SHOPPINGBASKET}>Winkelwagen</Link></a>
             </div>
           </div>
         : <Link className="nav-link" to={Routes.AUTH_SIGN_IN}>Log in</Link>
